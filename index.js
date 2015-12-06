2406a0dba96f1fa067e47b81f16b69bf8ea00bc8
faure <hi@faure.hu>
2015-12-06 07:13:15 +0000

    wtf

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function normalizePort(val) {
   return false;
 }
 
-var port = normalizePort(process.env.PORT || '3000');
+var port = process.env.PORT || '3000';
 
 server.connection({
     host: 'localhost',
